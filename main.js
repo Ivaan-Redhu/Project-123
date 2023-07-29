4cdd28498a9cd64c2ca678d611305db46ecd8c3f
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-07-30 11:02:11 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ function setup() {
 
     canvas = createCanvas(550, 500);
     canvas.position(560, 500);
-
     poseNet = ml5.poseNet(video, modelLoaded);
-
-    function modelLoaded() {
-        console.log('PoseNet Is Intianalized');
     }
-}
+    function modelLoaded() {
+        console.log('PoseNet Is Intianalized!');
+    }
+    function gotPoses(results)
+    {
+        if(results.length > 0)
+        {
+            console.log(results);
+
+            leftWristX = results[0].pose.leftWrist.x;
+            rightWristX = results[0 ].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWristX);
+        }
+    }
+
+    function draw() {
+        background('#6C91C2')
+
+        textSize(difference);
+        fill('#FFE787');
+        text('Ivaan', 50, 400);
+    }
