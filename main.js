092e45b7728b9d58f07c8c55dc73f5e2013d3a39
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-08-13 17:23:08 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftwristX= 0;
+rightWristX= 0;
+difference= 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -5,6 +9,7 @@ function setup() {
     canvas = createCanvas(550, 500);
     canvas.position(560, 500);
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
     }
     function modelLoaded() {
         console.log('PoseNet Is Intianalized!');
@@ -23,7 +28,7 @@ function setup() {
 
     function draw() {
         background('#6C91C2')
-
+        document.getElementById('font_size').innerHTML="Font size of The Text will be: " + difference + "px";
         textSize(difference);
         fill('#FFE787');
         text('Ivaan', 50, 400);
